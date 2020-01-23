ef4e46e77610a900e35c93a6bbdfcfc846fd3eec
Alex Coronell <alexcoronell@gmail.com>
2020-01-22 23:21:18 -0500

    Se agregan fuentes, font-awesome y se termina menu versión mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $('document').ready(function() {
-    
+
+    /* Mostrar y ocultar menu versión mobile */
+    $('#boton-menu').click(function() {
+        $('.menu').removeClass('menu-oculto');
+    });
+    $('.esconder-menu').click(function() {
+        $('.menu').addClass('menu-oculto');
+    });
+
 })
