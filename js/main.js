06ab454fef66ddfe09bb693069fdc99bf2a458bf
Alex Coronell <alexcoronell@gmail.com>
2020-01-24 00:55:50 -0500

    Terminado menu versión móvil y desktop

--- js/main.js
+++ js/main.js
@@ -2,10 +2,26 @@ $('document').ready(function() {
 
     /* Mostrar y ocultar menu versión mobile */
     $('#boton-menu').click(function() {
-        $('.menu').removeClass('menu-oculto');
+        if ($(window).width() < 768) {
+            $('.menu').removeClass('menu-oculto');
+        } else {
+        $('#boton-menu').addClass('ocultar-boton-menu');
+        $('#boton-cerrar-menu').addClass('ocultar-menu');
+        $('#boton-cerrar-menu').removeClass('ocultar-boton-menu');
+        $('.menu').addClass('menu-desktop');
+        }
     });
+    
     $('.esconder-menu').click(function() {
-        $('.menu').addClass('menu-oculto');
+            $('.menu').addClass('menu-oculto');
     });
 
-})
+    $('#boton-cerrar-menu').click(function() {
+        $('#boton-menu').removeClass('ocultar-boton-menu');
+        $('#boton-cerrar-menu').removeClass('ocultar-menu');
+        $('#boton-cerrar-menu').addClass('ocultar-boton-menu');
+        $('.menu').removeClass('menu-desktop');
+    });
+
+
+});
